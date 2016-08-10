38187f3cb4353dd5280268cde497201eafd14b00
Bryan Hankins <bryanhankins@me.com>
2016-08-10 15:48:38 -0500

    descending order

---
+++ chaining.js
@@ -0,0 +1,26 @@
+var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+
+// Sort the numbers in descending order (10, 9, 8, 7, etc).
+  var descending = integers.sort(function(a,b) {
+    return b-a
+  })
+  console.log(descending);
+
+// Remove any integers greater than 19.
+
+// Multiply each remaining number by 1.5 and then subtract 1.
+
+// Then output (either in the DOM or the console) the sum of all the resulting numbers.
+
+
+
+
+
+
+
+
+
+
+// Using one single line of JavaScript code, complete the following tasks on the array of integers below.
+
+// var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
