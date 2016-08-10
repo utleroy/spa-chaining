8dfb1e8c2fb6fc2f9180e9f78381999c4bfd0a49
Bryan Hankins <bryanhankins@me.com>
2016-08-10 16:22:20 -0500

    removed greater than 19 and did the multiply by 1.5

--- chaining.js
+++ chaining.js
@@ -4,11 +4,23 @@ var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
   var descending = integers.sort(function(a,b) {
     return b-a
   })
-  console.log(descending);
+  console.log("Sort the numbers in descending order (10, 9, 8, 7, etc).",descending);
 
-// Remove any integers greater than 19.
+
+for (var i = 0; i < integers.length; i++) {
+  if(integers[i] >= 19) {
+    integers.splice(i, 4);
+  }
+}
+
+console.log("Remove any integers greater than 19.", descending)
 
 // Multiply each remaining number by 1.5 and then subtract 1.
+
+var multiply = integers.map(function(num) {
+  return num * 1.5 - 1;
+})
+console.log("Multiply each remaining number by 1.5 and then subtract 1.", multiply)
 
 // Then output (either in the DOM or the console) the sum of all the resulting numbers.
 
